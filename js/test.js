300c2ea09b7962f9cfa59fc523324b3a59d35b17
Ruslan Sudenko <ruslansudenko@gmail.com>
2023-12-06 17:58:40 +0000

    there

--- js/test.js
+++ js/test.js
@@ -1,1 +1,13 @@
-c
+function createArrayOfNumbers(min, max) {
+  const numbers = [];
+  // Change code below this line
+  for (let i = min; i <= max; i += 1) {
+    numbers.push(i);
+    console.log(i);
+  }
+  // Change code above this line
+  return numbers;
+}
+console.log(10);
+
+createArrayOfNumbers(1, 5);
